8580fcc53d13f72681e9c56902ecdf29d94c1552
Nathan Gabriel <nategabrielk@icloud.com>
2025-06-19 16:22:49 -0400

    updated user-favorites

--- features/R9/test-sample.sql
+++ features/R9/test-sample.sql
@@ -5,3 +5,9 @@ INSERT INTO user VALUES ("a11", "uwaterloo_student", "password", FALSE)
 -- Which movies the user selected as their favorites
 INSERT INTO favorites VALUES ("a11", "tt0468569")
 
+
+SELECT t.tconst, t.primary_title, t.release_year, t.average_rating 
+FROM favorites f
+JOIN title t ON f.tconst = t.tconst
+WHERE f.userid = 'a11'
+ORDER BY t.primary_title
