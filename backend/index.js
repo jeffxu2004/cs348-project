8b0ad405bdb95dd097bd6fc1cd21570a5fad3048
Nathan Gabriel <nategabrielk@icloud.com>
2025-07-22 17:14:54 -0400

    fixed CORS missing code

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,7 @@ await fastify.register(import("@fastify/cookie"), {
 await fastify.register(cors, {
     origin: "http://localhost:5173",
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 });
 
 // Register routes
