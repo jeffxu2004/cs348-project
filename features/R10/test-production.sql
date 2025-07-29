fa19a5dfab5e6cdd817da2cbae288ffc8cec26f9
Nathan Gabriel <nategabrielk@icloud.com>
2025-07-29 12:11:52 -0400

    udpated code for R10

--- features/R10/test-production.sql
+++ features/R10/test-production.sql
@@ -1,15 +1,19 @@
 USE movie_app;
-/*
-INSERT INTO
-    reviews (userid, tconst, content)
-VALUES
-    (
-        'u1',
-        'tt0068646',
-        'this movie rocked! i really enjoyed frame #3193'
-    );*/
 
-SELECT
-    *
-FROM
-    reviews r;
+INSERT INTO reviews (userid, tconst, content) VALUES
+    ('u1', 'tt0068646', 'this movie rocked! i really enjoyed frame #3193'),
+    ('a01', 'tt0068646', 'I would recomend to friends'),
+    ('u00', 'tt0068646', 'I do no see the hype.')
+ON DUPLICATE KEY UPDATE content = VALUES(content);
+
+SELECT r.userid, r.content, u.username
+FROM reviews r
+JOIN user u ON r.userid = u.userid
+WHERE r.tconst = 'tt0068646'
+ORDER BY u.username;
+
+
+SELECT content FROM reviews WHERE tconst = 'tt0068646' AND userid = 'u1';
+
+DELETE FROM reviews WHERE tconst = 'tt0068646' AND userid = 'u1';
+;
--- features/R10/test-sample.sql
+++ features/R10/test-sample.sql
@@ -1,18 +1,18 @@
 USE movie_app;
 
-INSERT INTO
-    reviews (userid, tconst, content)
-VALUES
-    (
-        'user0',
-        'tt0068646',
-        'this movie rocked! i really enjoyed frame #3193'
-    );
+INSERT INTO reviews (userid, tconst, content) VALUES
+    ('u1', 'tt0068646', 'this movie rocked! i really enjoyed frame #3193'),
+    ('u2', 'tt0068646', 'I would recomend to friends'),
+    ('u3', 'tt0068646', 'I do no see the hype.')
+ON DUPLICATE KEY UPDATE content = VALUES(content);
 
-SELECT
-    r.*
-FROM
-    reviews r,
-    users u
-WHERE
-    r.userid = u.userid;
+SELECT r.userid, r.content, u.username
+FROM reviews r
+JOIN user u ON r.userid = u.userid
+WHERE r.tconst = 'tt0068646'
+ORDER BY u.username;
+
+
+SELECT content FROM reviews WHERE tconst = 'tt0068646' AND userid = 'u1';
+
+DELETE FROM reviews WHERE tconst = 'tt0068646' AND userid = 'u1';
