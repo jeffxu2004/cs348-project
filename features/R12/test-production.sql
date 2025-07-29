598d9aa456cc26b3da255b5503162fc1e86d4dba
Nathan Gabriel <nategabrielk@icloud.com>
2025-07-29 13:29:19 -0400

    updated code for R12

--- features/R12/test-production.sql
+++ features/R12/test-production.sql
@@ -1,5 +1,27 @@
 USE movie_app;
 
-ALTER TABLE title
-  ADD FULLTEXT idx_title_plot (primary_title, plot);
+SELECT 
+  tconst,
+  primary_title,
+  numvotes,
+  average_rating,
+  plot,
+  MATCH(primary_title, plot) AGAINST ('A yound man fighting crime' IN NATURAL LANGUAGE MODE) AS relevance
+FROM title
+WHERE MATCH(primary_title, plot) AGAINST ('A yound man fighting crime' IN NATURAL LANGUAGE MODE)
+ORDER BY relevance DESC
+LIMIT 20
 ;
+
+
+SELECT 
+tconst,
+primary_title,
+numvotes,
+average_rating,
+plot
+FROM title
+WHERE primary_title LIKE '%Dark knight%'
+LIMIT 20
+;
+
