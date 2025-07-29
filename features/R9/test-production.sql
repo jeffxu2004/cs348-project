2d49909b07ed57baa467459ccc3c1c8e25fbbefb
Nathan Gabriel <nategabrielk@icloud.com>
2025-07-29 11:02:07 -0400

    updated R9 queries

--- features/R9/test-production.sql
+++ features/R9/test-production.sql
@@ -1,13 +1,18 @@
 USE movie_app;
--- Note no changes were made since we are working with the same of toy dataset
--- Adding a new user
-INSERT INTO user VALUES ("a11", "uwaterloo_student", "password", FALSE)
+
+-- Find login based on username (to allow user to login)
+SELECT userid, username, password, isAdmin FROM user WHERE username = 'jesus81u88503';
+
+-- Favorite
 -- Which movies the user selected as their favorites
-INSERT INTO favorites VALUES ("a11", "tt0468569")
+INSERT INTO favorites VALUES ("u88503", "tt0167260");
 
-
+-- view the list of all movies in the user's favorite list
 SELECT t.tconst, t.primary_title, t.release_year, t.average_rating 
 FROM favorites f
 JOIN title t ON f.tconst = t.tconst
-WHERE f.userid = 'a11'
-ORDER BY t.primary_title
+WHERE f.userid = 'u88503'
+ORDER BY t.primary_title;
+
+
+DELETE FROM favorites WHERE tconst = 'tt0167260' AND userid = 'u88503';
--- features/R9/test-sample.sql
+++ features/R9/test-sample.sql
@@ -1,13 +1,21 @@
 USE movie_app;
 
 -- Adding a new user
-INSERT INTO user VALUES ("a11", "uwaterloo_student", "password", FALSE)
+INSERT INTO user VALUES ("a11", "uwaterloo_student", "password", FALSE);
+
+-- Find login based on username (to allow user to login)
+SELECT userid, username, password, isAdmin FROM user WHERE username = 'uwaterloo_student';
+
+-- Favorite
 -- Which movies the user selected as their favorites
-INSERT INTO favorites VALUES ("a11", "tt0468569")
+INSERT INTO favorites VALUES ("a11", "tt0468569");
 
 
 SELECT t.tconst, t.primary_title, t.release_year, t.average_rating 
 FROM favorites f
 JOIN title t ON f.tconst = t.tconst
 WHERE f.userid = 'a11'
-ORDER BY t.primary_title
+ORDER BY t.primary_title;
+
+
+DELETE FROM favorites WHERE tconst = 'a11' AND userid = 'tt0468569';
