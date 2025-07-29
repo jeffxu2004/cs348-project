6473e7b67ec82bae6b9b7f5b56821db481fd70f1
Alan Tang <alan.tang.astro@gmail.com>
2025-07-29 17:47:11 -0400

    fix

--- features/R14/test-sample.sql
+++ features/R14/test-sample.sql
@@ -1,15 +1,15 @@
-WITH example_embedding AS (
-    SELECT Vec_ToText(embedding) FROM title WHERE tconst = 'tt0068646'
+WITH target_embedding AS (
+    SELECT embedding FROM title WHERE tconst = 'tt0068646'
 )
 SELECT 
     tconst,
     primary_title,
     average_rating,
     plot,
-    VEC_DISTANCE_COSINE(embedding, Vec_FromText(example_embedding)) AS similarity_score
+    VEC_DISTANCE_COSINE(embedding, (SELECT embedding FROM target_embedding)) AS similarity_score
 FROM title t
 WHERE embedding IS NOT NULL
-AND VEC_DISTANCE_COSINE(embedding, Vec_FromText(example_embedding)) < 0.7
-ORDER BY VEC_DISTANCE_COSINE(embedding, Vec_FromText(example_embedding)) ASC
+AND VEC_DISTANCE_COSINE(embedding, (SELECT embedding FROM target_embedding)) < 0.7
+ORDER BY VEC_DISTANCE_COSINE(embedding, (SELECT embedding FROM target_embedding)) ASC
 LIMIT 10;
 
